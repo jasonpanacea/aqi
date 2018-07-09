e730e84e745dedeaf71d09cba0ce6433206f0991
jasonpanacea <f6434382@126.com>
2018-07-09 22:36:17 +0800

    fix

--- src/components/WholeCountry.js
+++ src/components/WholeCountry.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactEcharts from 'echarts-for-react';
 import { observer } from 'mobx-react';
 import 'echarts/extension/bmap/bmap';
-import { Spin, Card } from 'antd';
+import { Spin, Card, Modal } from 'antd';
 
 import BaseStore from '../stores/BaseStore';
+
+import '../styles/WholeCountry.less';
 
 
 @observer
@@ -18,14 +20,13 @@ export default class WholeCountry extends React.Component {
     };
   }
 
-  componentWillReceiveProps() {
-
+  componentWillUpdate(nextProps, nextState) {
+    console.log(nextProps);
+    console.log(nextState);
   }
- 
-  getOption = (center = [], zoomLevel = 5) => {
-    if (center.length === 0) {
-      center = [104.114129, 37.550339];
-    }
+
+
+  getOption = (center = [104.114129, 37.550339], zoomLevel = 5) => {
     const option = {
       title: {
         text: '全国主要城市AQHI',
@@ -144,6 +145,8 @@ export default class WholeCountry extends React.Component {
     const bmap = echarts_instance.getModel().getComponent('bmap').getBMap();
     const center = bmap.getCenter();
     echarts_instance.setOption(this.getOption([center.lng, center.lat], bmap.getZoom()));
+    this.center = [center.lng, center.lat];
+    this.zoomLevel = bmap.getZoom();
   }
 
   click = (params) => {
@@ -168,13 +171,13 @@ export default class WholeCountry extends React.Component {
       <div className="map">
         <ReactEcharts
           ref={(e) => { this.echarts_react = e; }}  
-          option={this.getOption(this.props.center, this.props.zoomLevel)}
+          option={this.getOption(this.center, this.zoomLevel)}
           style={{ height: '650px', width: '100%' }}
           className="react_for_echarts"
           onEvents={onEvents}
         />
         { this.state.cardVisible &&
-        <Card title="Card title" style={{ width: 300, position: 'fixed', right: '20px', bottom: '10px', zIndex: 99999, opacity:0.5 }}>
+        <Card title="Card title" style={{ width: 300, position: 'fixed', right: '20px', bottom: '10px', zIndex: 99999, opacity: 0.5 }}>
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>
